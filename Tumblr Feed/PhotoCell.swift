031b0fc7270bda30c4fe772807a43c492170db07
johnzoldos <john.a.zoldos@gmail.com>
2017-05-30 16:50:21 -0400

    App now shows Humans of New York feed

---
+++ Tumblr Feed/PhotoCell.swift
@@ -0,0 +1,27 @@
+//
+//  PhotoCell.swift
+//  Tumblr Feed
+//
+//  Created by John Zoldos on 5/30/17.
+//  Copyright © 2017 John Zoldos. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCell: UITableViewCell {
+
+    @IBOutlet weak var photoView: UIImageView!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
